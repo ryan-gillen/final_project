967b3c6751eacf9bfb5266e76f14e4e95cebb7d6
Alexandra Krall <alexandra.krall@kellogg.northwestern.edu>
2021-03-10 21:26:17 -0600

    starting javascript code

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 firebase.auth().onAuthStateChanged(async function(user) {
+
+
   if (user) {
     // Signed in
     console.log('signed in')
+
+
+    // ADDING SIGNOUT BUTTON - AK ADDED 3.10 (NOT GIVEN IN STARTER CODE)
+    document.querySelector('.sign-in-or-sign-out').innerHTML = `
+      <button class="text-blue-500 underline sign-out">Sign Out</button>
+    `
+    document.querySelector('.sign-out').addEventListener('click', function(event) {
+      console.log('sign out clicked')
+      firebase.auth().signOut()
+      document.location.href = 'index.html'
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   } else {
     // Signed out
     console.log('signed out')
+
+    //HIDE FROM WHEN SIGNED-OUT- AK ADDED 3.10 (NOT GIVEN IN STARTER CODE)
+    document.querySelector('form').classList.add('hidden')
 
     // Initializes FirebaseUI Auth
     let ui = new firebaseui.auth.AuthUI(firebase.auth())
