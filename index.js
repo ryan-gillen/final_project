202048ce6796d638fb443b6d60cfad724854a0e9
Ryan Gillen <77376990+ryan-gillen@users.noreply.github.com>
2021-03-13 15:26:07 -0600

    commit 2

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-//NOT GIVEN IN STARTER CODE
-let db = firebase.firestore()
-
-//STARTER CODE
 firebase.auth().onAuthStateChanged(async function(user) {
-
-
   if (user) {
     // Signed in
     console.log('signed in')
 
-    // RG: Ensure the signed-in user is in the users collection (not sure if we'll need this in the end?)
-    db.collection('users').doc(user.uid).set({
-      name: user.displayName,
-      email: user.email
-    })
-
-//CODE ADDED 3.10 & 3/12 - LEVERAGED WEEK 7 LAB for reference
-//NOT GIVEN IN STARTER CODE
-
-  // AK ADDED 3.12 
-  // Ensures the signed-in user is in the users collection - NOT SURE IF IT IS NEEDED 
-    db.collection('users').doc(user.uid).set({
-      name: user.displayName,
-      email: user.email
-        })
-
-    // ADDING SIGNOUT BUTTON - AK ADDED 3.10 
+    // Sign-out button
     document.querySelector('.sign-in-or-sign-out').innerHTML = `
-      <button class="text-blue-500 underline sign-out">Sign Out</button>
+      <button class="text-pink-500 underline sign-out">Sign Out</button>
     `
     document.querySelector('.sign-out').addEventListener('click', function(event) {
       console.log('sign out clicked')
@@ -35,62 +13,39 @@ firebase.auth().onAuthStateChanged(async function(user) {
       document.location.href = 'index.html'
     })
 
-    // Listen for the form submit and create/render the new post - ADDED AK 3.12
-     document.querySelector('form').addEventListener('submit', async function(event) {
+    // Listen for the form submit and create/render the new post
+    document.querySelector('form').addEventListener('submit', async function(event) {
       event.preventDefault()
-      let description = document.querySelector('#description').value
-      let link = document.querySelector('#link').value
-      let image = document.querySelector('#image').value
-      let submitter = document.querySelector('#submitter').value
-      let numberupvotes = 0
-      let numberdownvotes = 0
-      let docRef = await db.collection('ideas').add({ 
-        ideadescription: description, 
-        idealink: link, 
-        ideaimage: image,
-        ideasubmitter: submitter,
-        upvotes: 0,
-        downvotes: 0, 
-        created: firebase.firestore.FieldValue.serverTimestamp()
+      let postUsername = user.displayName
+      let postImageUrl = document.querySelector('#image-url').value
+      let response = await fetch('/.netlify/functions/create_post', {
+        method: 'POST',
+        body: JSON.stringify({
+          userId: user.uid,
+          username: postUsername,
+          imageUrl: postImageUrl
+        })
       })
-
-    //newly created documents - ADDED AK 3.12
-      letideaID = docRef.id 
-      renderIdea(ideaId, description, link, image, submitter, numberupvotes, numberdownvotes)
+      let post = await response.json()
+      document.querySelector('#image-url').value = '' // clear the image url field
+      renderPost(post)
     })
 
-  //Render ideas when the page it loaded - ADDED AK 3.12
-  let querySelector = await db.collection('ideas').orderBy('created').get()
-  let posts = querySnapshot.docs
-  for (let i=0; i<ideas.length; i++) {
-    let ideaId = ideas[i].id
-    let ideaData = ideas[i].data()
-    let description = ideaData.description
-    let link = ideaData.link
-    let image = ideaData.image
-    let submitter = ideaData.submitter
-    let numberupvotes = ideaData.upvotes
-    let numberdownvotes = ideaData.downvotes
-    renderIdea(ideaId, description, link, image, submitter, numberupvotes, numberdownvotes)
-
-  }
-
-
-// STARTER CODE 
-
+    let response = await fetch('/.netlify/functions/get_posts')
+    let posts = await response.json()
+    for (let i=0; i<posts.length; i++) {
+      let post = posts[i]
+      renderPost(post)
+    }
   } else {
     // Signed out
     console.log('signed out')
 
-    //HIDE FROM WHEN SIGNED-OUT- AK ADDED 3.10 
-    //NOT GIVEN IN STARTER CODE)
+    // Hide the form when signed-out
     document.querySelector('form').classList.add('hidden')
-
-  //STATER CODE 
 
     // Initializes FirebaseUI Auth
     let ui = new firebaseui.auth.AuthUI(firebase.auth())
-    //console.log ('firebase new auth success')
 
     // FirebaseUI configuration
     let authUIConfig = {
@@ -99,55 +54,129 @@ firebase.auth().onAuthStateChanged(async function(user) {
       ],
       signInSuccessUrl: 'index.html'
     }
-    //console.log ('firebase auth config sucess')
-    
 
     // Starts FirebaseUI Auth
     ui.start('.sign-in-or-sign-out', authUIConfig)
-    console.log ('start firebaseUI auth success')
-
   }
 })
 
-//NOT GIVEN IN STARTER CODE - 3/12 AK - NEED TO PLAY AROUND WITH THIS MORE 
-//how do we show the right description, link, image, smubittor,vote - do we link to the ID??? 
-//For EXAMPLE - ${ideaID}-${description} ??
+// given a single post Object, render the HTML and attach event listeners
+// expects an Object that looks similar to:
+// {
+//   id: 'abcdefg',
+//   username: 'brian',
+//   imageUrl: 'https://images.unsplash.com/...',
+//   likes: 12,
+//   comments: [
+//     { username: 'brian', text: 'i love tacos!' },
+//     { username: 'ben', text: 'fake news' }
+//   ]
+// }
+async function renderPost(post) {
+  let postId = post.id
+  document.querySelector('.posts').insertAdjacentHTML('beforeend', `
+    <div class="post-${postId} md:mt-16 mt-8 space-y-8">
+      <div class="md:mx-0 mx-4">
+        <span class="font-bold text-xl">${post.username}</span>
+      </div>
 
-async function renderIdea(ideaId, description, link, image, submitter, numberupvotes, numberdownvotes) {
-  document.querySelector('.Submitted').insertAdjacentHTML('beforeend', `
-  <div id="description" class="p-2">
-  <p>$${description}</p>
-</div>
+      <div>
+        <img src="${post.imageUrl}" class="w-full">
+      </div>
 
-<div id="link" class="p-2">
-  <a href>${link}</a>
-</div>
+      <div class="text-3xl md:mx-0 mx-4">
+        <button class="like-button">❤️</button>
+        <span class="likes">${post.likes}</span>
+      </div>
 
-<div id="image" class="p-2">
-  <img src = ${image}> 
-</div>
+      <div class="comments text-sm md:mx-0 mx-4 space-y-2">
+        ${renderComments(post.comments)}
+      </div>
 
-<div id="submitter" class="p-2 italic">
-  <p>${submitter}</p>
-</div>
+      <div class="w-full md:mx-0 mx-4">
+        ${renderCommentForm()}
+      </div>
+    </div>
+  `)
 
-<div class="tripvote p-2">
-  <form>
-      <button> 👍  </button> <a id="${numberupvotes}">  </a><button> 👎  </button><a id="${numberdownvotes}"> </a>
-  </form>
-</div>`
-  )
+  // listen for the like button on this post
+  let likeButton = document.querySelector(`.post-${postId} .like-button`)
+  likeButton.addEventListener('click', async function(event) {
+    event.preventDefault()
+    console.log(`post ${postId} like button clicked!`)
+    let currentUserId = firebase.auth().currentUser.uid
 
+    let response = await fetch('/.netlify/functions/like', {
+      method: 'POST',
+      body: JSON.stringify({
+        postId: postId,
+        userId: currentUserId
+      })
+    })
+    if (response.ok) {
+      let existingNumberOfLikes = document.querySelector(`.post-${postId} .likes`).innerHTML
+      let newNumberOfLikes = parseInt(existingNumberOfLikes) + 1
+      document.querySelector(`.post-${postId} .likes`).innerHTML = newNumberOfLikes
+    }
+  })
 
-//   document.querySelector(`.post-${postId} .like-button`).addEventListener('click', async function(event) {
-//     event.preventDefault()
-//     console.log(`post ${postId} like button clicked!`)
-//     let existingNumberOfLikes = document.querySelector(`.post-${postId} .likes`).innerHTML
-//     let newNumberOfLikes = parseInt(existingNumberOfLikes) + 1
-//     document.querySelector(`.post-${postId} .likes`).innerHTML = newNumberOfLikes
-//     await db.collection('posts').doc(postId).update({
-//       likes: firebase.firestore.FieldValue.increment(1)
-//     })
-//   })
+  // listen for the post comment button on this post
+  let postCommentButton = document.querySelector(`.post-${postId} .post-comment-button`)
+  postCommentButton.addEventListener('click', async function(event) {
+    event.preventDefault()
+    console.log(`post ${postId} post comment button clicked!`)
 
+    // get the text of the comment
+    let postCommentInput = document.querySelector(`.post-${postId} input`)
+    let newCommentText = postCommentInput.value
+    console.log(`comment: ${newCommentText}`)
+
+    // create a new Object to hold the comment's data
+    let newComment = {
+      postId: postId,
+      username: firebase.auth().currentUser.displayName,
+      text: newCommentText
+    }
+
+    // call our back-end lambda using the new comment's data
+    await fetch('/.netlify/functions/create_comment', {
+      method: 'POST',
+      body: JSON.stringify(newComment)
+    })
+
+    // insert the new comment into the DOM, in the div with the class name "comments", for this post
+    let commentsElement = document.querySelector(`.post-${postId} .comments`)
+    commentsElement.insertAdjacentHTML('beforeend', renderComment(newComment))
+
+    // clears the comment input
+    postCommentInput.value = ''
+  })
+}
+
+// given an Array of comment Objects, loop and return the HTML for the comments
+function renderComments(comments) {
+  if (comments) {
+    let markup = ''
+    for (let i = 0; i < comments.length; i++) {
+      markup += renderComment(comments[i])
+    }
+    return markup
+  } else {
+    return ''
   }
+}
+
+// return the HTML for one comment, given a single comment Object
+function renderComment(comment) {
+  return `<div><strong>${comment.username}</strong> ${comment.text}</div>`
+}
+
+// return the HTML for the new comment form
+function renderCommentForm() {
+  let commentForm = ''
+  commentForm = `
+    <input type="text" class="mr-2 rounded-lg border px-3 py-2 focus:outline-none focus:ring-purple-500 focus:border-purple-500" placeholder="Add a comment...">
+    <button class="post-comment-button py-2 px-4 rounded-md shadow-sm font-medium text-white bg-purple-600 focus:outline-none">Post</button>
+  `
+  return commentForm
+}
